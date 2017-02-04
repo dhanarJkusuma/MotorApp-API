484c23c21ad3ccd2a2db6ae834294b9ee5fbc088
Dhanar J Kusuma <Dhanar J Kusuma>
2017-02-04 23:30:17 +0700

    finish.

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -4,7 +4,7 @@
 var multer = require('multer');
 var diskStorage = multer.diskStorage({
     destination : function(req, file, cb){
-        cb(null, 'public/uploads/category');
+        cb(null, 'public/uploads/category/');
     },
     filename : function(req, file, cb){
         cb(null, Date.now() + "_" + file.originalname);
